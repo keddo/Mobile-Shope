6ade63803959895da5d8de4b037441f0bc799271
keddo <adu.kedir.abdurahman@gmail.com>
2020-05-18 11:56:15 +0300

    Add ads and new phones section

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,4 +28,16 @@ $(document).ready(function(){
         var filterValue = $(this).attr('data-filter');
         $grid.isotope({filter: filterValue});
     })
+
+    // new Phones 
+    $('#new-phones .owl-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: { items: 1},
+            600: {items: 3},
+            1000: {item: 5}
+        }
+    });
 });
